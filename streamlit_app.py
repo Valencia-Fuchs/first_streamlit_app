5bff8de4f73b71fd50e8f959bc2ce5b319836c2b
Juan M. Valencia <46378809+Valencia-Fuchs@users.noreply.github.com>
2022-09-10 16:26:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title('My Parents New Healthy Dinner') 
 st.header('Breakfast Menu')
-st.text('Omega & and Blueberry Oatmeal\n
-         Kale, Spinach & Rocket Smoothie\n
-         Hard-Boiled Free-Range Egg')
+st.text('Omega & and Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')   
