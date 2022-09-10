817e421c2f6656d543530fc83df3c1db0119d48a
Juan M. Valencia <46378809+Valencia-Fuchs@users.noreply.github.com>
2022-09-10 16:25:34 +0200

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Dinner') 
+st.title('My Parents New Healthy Dinner') 
+st.header('Breakfast Menu')
+st.text('Omega & and Blueberry Oatmeal\n
+         Kale, Spinach & Rocket Smoothie\n
+         Hard-Boiled Free-Range Egg')
